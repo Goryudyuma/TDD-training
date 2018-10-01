54577de866953d7783c8b572e4269bbad1a66e4f
Goryudyuma <umaretekyoumade@gmail.com>
2018-10-02 00:27:53 +0900

    add comment

--- 1/money.go
+++ 1/money.go
@@ -1,14 +1,17 @@
 package money
 
+// Dollar : ドル
 type Dollar struct {
 	Amount int
 }
 
+// NewDollar : 新しく(*Dollar)を作る
 func NewDollar(amount int) *Dollar {
 	ret := Dollar{}
 	return &ret
 }
 
+// Times : multipier倍にして返す
 func (dollar *Dollar) Times(multipier int) {
 
 }
