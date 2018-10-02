277f1d45878715329a41c840ee51ef51a1f0160f
Goryudyuma <umaretekyoumade@gmail.com>
2018-10-03 00:03:09 +0900

    fix test case

--- 1/money_test.go
+++ 1/money_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
-	five.Times(2)
-	if five.Amount != 10 {
+	product := five.Times(2)
+	if product.Amount != 10 {
 		t.Error(`$5 * 2 が $10じゃなかった`)
 	}
-	five.Times(3)
-	if five.Amount != 15 {
+	product = five.Times(3)
+	if product.Amount != 15 {
 		t.Error(`$5 * 3 が $15じゃなかった`)
 	}
 }
