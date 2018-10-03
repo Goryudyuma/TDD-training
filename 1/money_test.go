3b1cd7d3a53b726d53603e06d1a3771ba371bc19
Goryudyuma <umaretekyoumade@gmail.com>
2018-10-04 03:50:27 +0900

    add test case

--- 1/money_test.go
+++ 1/money_test.go
@@ -1,6 +1,8 @@
 package money
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
@@ -11,5 +13,11 @@ func TestMultiplication(t *testing.T) {
 	product = five.Times(3)
 	if product.Amount != 15 {
 		t.Error(`$5 * 3 が $15じゃなかった`)
+	}
+}
+
+func TestEquality(t *testing.T) {
+	if !NewDollar(5).Equals(NewDollar(5)) {
+		t.Error(`$5 と $5 が等価ではなかった`)
 	}
 }
