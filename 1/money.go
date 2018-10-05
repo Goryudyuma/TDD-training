65bb46c67660f5d573fa4c442c66302fc591e62f
Goryudyuma <umaretekyoumade@gmail.com>
2018-10-05 21:57:45 +0900

    fix amount to private

--- 1/money.go
+++ 1/money.go
@@ -2,21 +2,21 @@ package money
 
 // Dollar : ドル
 type Dollar struct {
-	Amount int
+	amount int
 }
 
 // NewDollar : 新しく(*Dollar)を作る
 func NewDollar(amount int) *Dollar {
-	ret := Dollar{Amount: amount}
+	ret := Dollar{amount: amount}
 	return &ret
 }
 
 // Times : multipier倍にして返す
 func (dollar *Dollar) Times(multipier int) *Dollar {
-	return NewDollar(dollar.Amount * multipier)
+	return NewDollar(dollar.amount * multipier)
 }
 
 // Equals : 等価かどうか返す
 func (dollar *Dollar) Equals(another *Dollar) bool {
-	return dollar.Amount == another.Amount
+	return dollar.amount == another.amount
 }
