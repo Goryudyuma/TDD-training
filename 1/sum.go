e5d7c7d18397125a93e6554d09052aac6fb312f8
Goryudyuma <umaretekyoumade@gmail.com>
2018-11-11 23:27:51 +0900

    update Sum

--- 1/sum.go
+++ 1/sum.go
@@ -2,5 +2,10 @@ package money
 
 // Sum : 足し算を表す構造体
 type Sum struct {
-	augend, addend Money
+	Augend, Addend *Money
+}
+
+// NewSum : 足し算を表す構造体を返す
+func NewSum(augend, addend *Money) *Sum {
+	return &Sum{Augend: augend, Addend: addend}
 }
